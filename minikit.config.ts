c2ba9adbc57c648e13f9fdbd108e4c757660a12f
myreceiptt <nota@straight-line.org>
2025-11-30 04:58:34 +0700

    version must be 1

--- minikit.config.ts
+++ minikit.config.ts
@@ -21,7 +21,7 @@ export const minikitConfig = {
 
   // Bagian ini akan diubah jadi manifest "frame" oleh withValidManifest()
   miniapp: {
-    version: "v1.1.11",
+    version: "1",
     name: "MyReceipt",
     subtitle: "A small receipt to hold today.", // 30 chars ✅
     description:
@@ -41,7 +41,7 @@ export const minikitConfig = {
     webhookUrl: `${ROOT_URL}/api/webhook`,
 
     primaryCategory: "education",
-    tags: ["my-receipt-of-today", "nota", "reflection", "base", "prof-nota"],
+    tags: ["my-receipt-of-today", "nota", "reflection", "base", "prof-nota", "v1.1.11"],
 
     tagline: "Your onchain receipt of today.",
     ogTitle: "MyReceipt",
