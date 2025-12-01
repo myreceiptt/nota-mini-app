ae7738830ac0d206dee3ce4a2a22f5e80fc2e01b
myreceiptt <nota@straight-line.org>
2025-12-01 09:16:58 +0700

    baseBuilder

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -1,8 +1,22 @@
+// import { withValidManifest } from "@coinbase/onchainkit/minikit";
+// import { minikitConfig } from "../../../minikit.config";
+
+// export const dynamic = "force-static";
+
+// export async function GET() {
+//   return Response.json(withValidManifest(minikitConfig));
+// }
+
 import { withValidManifest } from "@coinbase/onchainkit/minikit";
 import { minikitConfig } from "../../../minikit.config";
 
-export const dynamic = "force-static";
-
 export async function GET() {
-  return Response.json(withValidManifest(minikitConfig));
+  const manifest = withValidManifest(minikitConfig);
+
+  return Response.json({
+    // output dari OnchainKit (accountAssociation + miniapp + apapun yang dia jaga)
+    ...manifest,
+    // pastikan baseBuilder SELALU ikut (kalau sudah ada pun, ini sekadar meng-override dengan value di config)
+    baseBuilder: minikitConfig.baseBuilder,
+  });
 }
